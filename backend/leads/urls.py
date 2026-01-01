6256a043286c0a933d10b652892d4b5312b2fd44
Shoxbro2007 <shoxbro2007@gmail.com>
2026-01-01 12:47:25 +0300

    fix: исправлены urlpatterns в urls.py

--- backend/leads/urls.py
+++ backend/leads/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('leads/', views.LeadListCreateView.as_view(), name='lead-list-create'),
+    path('token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
+
