e2c66dc766717b17a1a194a0f81c89b8c27a9f19
Shoxbro2007 <shoxbro2007@gmail.com>
2026-01-01 14:05:08 +0300

    fix: импортирован settings в urls.py

--- backend/crm/urls.py
+++ backend/crm/urls.py
@@ -1,21 +1,14 @@
+# crm/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf import settings  # ← Добавь эту строку!
+from django.conf.urls.static import static  # ← Если используешь static()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('leads.urls')),
-
-    # JWT авторизация
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    # API Schema
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
+# Только в DEBUG режиме (локально)
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
