f57d35db847bd7d446f77a1973e282d26985effc
Shoxbro2007 <shoxbro2007@gmail.com>
2026-01-02 16:21:58 +0300

    fix: исправлен render.yaml и путь к dist

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,13 @@
-// vite.config.ts
+// frontend/vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
+  build: {
+    outDir: 'dist', // ✅ Папка сборки
   },
 });
+
 
 
